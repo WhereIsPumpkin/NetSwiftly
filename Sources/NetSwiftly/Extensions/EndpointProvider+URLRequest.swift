754d41f05dbc555ebb03c05febd846cc2e6c3629
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2024-02-29 18:33:13 +0400

    add default values for endpointprovider

--- Sources/NetSwiftly/Extensions/EndpointProvider+URLRequest.swift
+++ Sources/NetSwiftly/Extensions/EndpointProvider+URLRequest.swift
@@ -1,20 +1,25 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Saba Gogrichiani on 29.02.24.
 //
 
 import Foundation
 
-extension EndpointProvider {
+public extension EndpointProvider {
+    var scheme: String { "https" }
+    var token: String? { nil }
+    var queryItems: [URLQueryItem]? { nil }
+    var body: [String: Any]? { nil }
+    
     func asURLRequest() throws -> URLRequest {
         guard let url = URL(string: "\(scheme)://\(baseURL)\(path)") else {
             throw ApiError(errorCode: "URL_CONSTRUCTION_FAILURE", message: "Unable to construct the URL")
         }
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = method.rawValue
-
+        
         // Headers
         urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
         urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -22,18 +27,18 @@ extension EndpointProvider {
         if let token = token {
             urlRequest.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         }
-
+        
         // Body
         if let body = body, let httpBody = try? JSONSerialization.data(withJSONObject: body, options: []) {
             urlRequest.httpBody = httpBody
         }
-
+        
         // Query Items
         if let queryItems = queryItems, var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) {
             urlComponents.queryItems = queryItems
             urlRequest.url = urlComponents.url
         }
-
+        
         return urlRequest
     }
 }
