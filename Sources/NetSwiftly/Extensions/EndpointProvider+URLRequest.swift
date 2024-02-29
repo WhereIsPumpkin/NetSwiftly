dbecfca7cd3e442a13677a5ad45a9661cd4733c3
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2024-02-29 17:19:14 +0400

    add EndpointProvider

---
+++ Sources/NetSwiftly/Extensions/EndpointProvider+URLRequest.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by Saba Gogrichiani on 29.02.24.
+//
+
+import Foundation
+
+extension EndpointProvider {
+    func asURLRequest() throws -> URLRequest {
+        guard let url = URL(string: "\(scheme)://\(baseURL)\(path)") else {
+            throw ApiError(errorCode: "URL_CONSTRUCTION_FAILURE", message: "Unable to construct the URL")
+        }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = method.rawValue
+
+        // Headers
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        if let token = token {
+            urlRequest.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        }
+
+        // Body
+        if let body = body, let httpBody = try? JSONSerialization.data(withJSONObject: body, options: []) {
+            urlRequest.httpBody = httpBody
+        }
+
+        // Query Items
+        if let queryItems = queryItems, var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) {
+            urlComponents.queryItems = queryItems
+            urlRequest.url = urlComponents.url
+        }
+
+        return urlRequest
+    }
+}
