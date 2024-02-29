83a4a2583c06eb0566cf84ec77bf9fadcedcf101
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2024-02-29 18:55:12 +0400

    add pulic for singleton instance

--- Sources/NetSwiftly/Networking/NetSwiftly.swift
+++ Sources/NetSwiftly/Networking/NetSwiftly.swift
@@ -6,7 +6,7 @@ import Foundation
 @available(iOS 13.0.0, *)
 public final class NetSwiftly {
     
-    static let shared = NetSwiftly()
+    public static let shared = NetSwiftly()
     
     private init() {}
     
