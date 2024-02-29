1761f15e28459bbebd033080f47306adb52e2be9
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2024-02-29 18:02:39 +0400

    add publics for making it accesible

--- Sources/NetSwiftly/Networking/NetSwiftly.swift
+++ Sources/NetSwiftly/Networking/NetSwiftly.swift
@@ -1,2 +1,30 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+@available(iOS 13.0.0, *)
+class NetSwiftly {
+    
+    static let shared = NetSwiftly()
+    
+    private init() {}
+    
+    func fetchData<T: Decodable>(from endpoint: EndpointProvider, responseType: T.Type) async throws -> T {
+        let request = try endpoint.asURLRequest()
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw ApiError(errorCode: "HTTP_ERROR", message: "Non-200 HTTP response")
+        }
+        
+        do {
+            let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+            return decodedResponse
+        } catch {
+            throw ApiError(errorCode: "DECODING_ERROR", message: "Failed to decode response")
+        }
+    }
+    
+    
+}
