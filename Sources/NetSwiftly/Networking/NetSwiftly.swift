31d17bc5d1a615ec0e22137f129b686b7875b738
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2024-03-01 19:34:16 +0400

    Enhance debugging logs with emojis for improved readability

--- Sources/NetSwiftly/Networking/NetSwiftly.swift
+++ Sources/NetSwiftly/Networking/NetSwiftly.swift
@@ -8,18 +8,45 @@ public final class NetSwiftly {
     
     public static let shared = NetSwiftly()
     
+    /// Debug flag
+    public var debugEnabled: Bool = false
+    
     private init() {}
     
     public func fetchData<T: Decodable>(from urlRequest: URLRequest, decodeTo type: T.Type) async throws -> T {
+        if debugEnabled {
+            print("🌐 Fetching data from: \(urlRequest.url?.absoluteString ?? "Unknown URL")")
+        }
+        
         let (data, response) = try await URLSession.shared.data(for: urlRequest)
         
-        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-            throw URLError(.badServerResponse)
+        if let httpResponse = response as? HTTPURLResponse {
+            if debugEnabled {
+                print("🚦 HTTP Status Code: \(httpResponse.statusCode)")
+            }
+            guard httpResponse.statusCode == 200 else {
+                if debugEnabled {
+                    print("❌ Error: Bad Server Response (\(httpResponse.statusCode))")
+                }
+                throw URLError(.badServerResponse)
+            }
         }
         
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .iso8601
-        return try decoder.decode(T.self, from: data)
+        
+        do {
+            let decodedData = try decoder.decode(T.self, from: data)
+            if debugEnabled {
+                print("✅ Successfully decoded \(T.self)")
+            }
+            return decodedData
+        } catch {
+            if debugEnabled {
+                print("❌ Decoding error: \(error)")
+            }
+            throw error
+        }
     }
     
 }
